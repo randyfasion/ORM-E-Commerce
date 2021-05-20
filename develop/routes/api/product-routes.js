5b0b5add66b3020e11901e5c0783711a2d1c952e
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-20 13:51:06 -0400

    all products route

--- develop/routes/api/product-routes.js
+++ develop/routes/api/product-routes.js
@@ -4,8 +4,16 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  try{
+    const allProducts = await Category.findAll({
+      include: [{ model: Product}],
+    });
+    res.status(200).json(allProducts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // be sure to include its associated Category and Tag data
 });
 
