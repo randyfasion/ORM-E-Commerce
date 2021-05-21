5262fad5c608cad6486b19cba31a49cb77807443
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-21 14:12:56 -0400

    post tags added

--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -35,8 +35,17 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+
+    const tagData = await Tag.create(req.body);
+
+    res.status(200).json(tagData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
