a91840a9c186ee4adee148c993a21cea60e25d98
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-21 14:06:41 -0400

    category delete route added

--- develop/routes/api/category-routes.js
+++ develop/routes/api/category-routes.js
@@ -63,8 +63,17 @@ router.put('/:id', async (req, res) => {
 });
 
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const allCategories = await Category.destroy({
+      where: { id: req.params.id}
+    });
+
+    res.status(200).json(allCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
