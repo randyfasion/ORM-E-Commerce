3a27dba8b20d9f87ff380a2ead0c6674ff62bd47
Randy Faison <randyfaison@randys-mbp.myfiosgateway.com>
2021-05-21 09:46:30 -0400

    find tag by id added

--- develop/routes/api/tag-routes.js
+++ develop/routes/api/tag-routes.js
@@ -16,8 +16,22 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Product data
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
+  try {
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{model: Product, through: ProductTag}],
+    });
+    if (!tagData) {
+      res.status(404).json({message: "No tag found with that id!"});
+      return;
+    }
+  
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+ 
   // be sure to include its associated Product data
 });
 
